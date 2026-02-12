5332bb44ce85f08307e63bab7fddb9773e85dea3
egilp04 <egilp04@iesalbarregas.es>
2026-02-12 17:49:59 +0100

    mi eprfil

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Diurno from "./components/Diurno";
+import Perfil from "./components/Perfil";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Route path="/" element={<Home></Home>}>
           <Route index element={<Diurno></Diurno>}></Route>
           <Route path="vespertino"></Route>
-          <Route path="perfil"></Route>
+          <Route path="perfil" element={<Perfil></Perfil>}></Route>
           <Route path="dias-solicitados"></Route>
           <Route path="ausencias"></Route>
         </Route>
