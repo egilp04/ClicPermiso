b3a3c51cad9766be69b839e021b814fed2155c21
egilp04 <egilp04@iesalbarregas.es>
2026-02-12 17:49:04 +0100

    ruta vespertino

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Diurno from "./components/Diurno";
+import Vespertino from "./components/Vespertino";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}>
           <Route index element={<Diurno></Diurno>}></Route>
-          <Route path="vespertino"></Route>
+          <Route path="vespertino" element={<Vespertino></Vespertino>}></Route>
           <Route path="perfil"></Route>
           <Route path="dias-solicitados"></Route>
           <Route path="ausencias"></Route>
